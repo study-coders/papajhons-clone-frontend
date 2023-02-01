c2a0589887007efd8e8d0cf3761de95e6e16c43e
alpetrushkin <alpetrushkin1984@mail.ru>
2023-02-01 20:07:53 +0300

    newCommit

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Drawer.scss'
+
+export const Drawer = () => {
+  return (
+    <div className="drawer__body">
+      <div className="block">
+        <div className="circle"></div>
+      </div>
+    </div>
+  )
+}
